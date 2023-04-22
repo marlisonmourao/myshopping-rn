7463aab052bcb823b50d8870b60308c4d3b7dcc3
marlisonmourao <bentesmourao@gmail.com>
2023-04-22 15:57:25 -0400

    logando user

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
+import auth from '@react-native-firebase/auth'
 
-// import { AppRoutes } from './app.routes'
 import { SignIn } from '../screens/SignIn'
+import { AppRoutes } from './app.routes'
+
+type User = {
+  uid: string
+}
 
 export function Routes() {
+  const [user, setUser] = useState<User | null>()
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged((user) => {
+      setUser(user)
+    })
+
+    return subscriber
+  }, [])
+
   return (
     <NavigationContainer>
-      <SignIn />
+      {user ? <AppRoutes /> : <SignIn />}
     </NavigationContainer>
   )
 }
